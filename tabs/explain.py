77369ebba222aa42d15cbdb48eb6066dbc1bc5f9
Ryan Herr <ryanherr@Ryans-MBP.zyxel.com>
2019-06-27 10:01:28 -0500

    Add partial dependence plot to explain tab

--- tabs/explain.py
+++ tabs/explain.py
@@ -4,10 +4,55 @@ import dash_html_components as html
 
 from app import app
 
-layout = dcc.Markdown("""
+import plotly.graph_objs as go
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble.partial_dependence import partial_dependence
+from sklearn.datasets.california_housing import fetch_california_housing
+
+cal_housing = fetch_california_housing()
+
+# split 80/20 train-test
+X_train, X_test, y_train, y_test = train_test_split(cal_housing.data,
+                                                    cal_housing.target,
+                                                    test_size=0.2,
+                                                    random_state=1)
+names = cal_housing.feature_names
+
+print("Training GBRT...")
+clf = GradientBoostingRegressor(n_estimators=100, max_depth=4,
+                                learning_rate=0.1, loss='huber',
+                                random_state=1)
+clf.fit(X_train, y_train)
+print(" done.")
+
+target_feature = (1, 5)
+pdp, axes = partial_dependence(clf, target_feature,
+                               X=X_train, grid_resolution=50)
+XX, YY = np.meshgrid(axes[0], axes[1])
+Z = pdp[0].reshape(list(map(np.size, axes))).T
+
+surf = go.Surface(x=XX, y=YY, z=Z)
+
+layout = go.Layout(title='Partial dependence of house value on median age and '
+                          'average occupancy',
+                   scene=dict(xaxis=dict(title=names[target_feature[0]],
+                                         showticklabels=False),
+                              yaxis=dict(title=names[target_feature[1]],
+                                         showticklabels=False),
+                              zaxis=dict(title='Partial dependence',
+                                         showticklabels=False))
+                   )
+fig = go.Figure(data = [surf], layout=layout)
+
+
+layout = [dcc.Markdown("""
 ### Explain
 
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer suscipit ornare dolor in venenatis. Nunc dapibus elementum ligula, sed pharetra turpis tempor vel. Pellentesque dui tellus, euismod vel libero in, volutpat pretium felis. Sed sit amet purus et tellus tincidunt finibus. Integer augue odio, pharetra at placerat ut, venenatis nec erat. Integer eu malesuada nisl. Nam vestibulum, elit quis pharetra luctus, massa nulla cursus augue, in tristique mi ipsum id nisi. Fusce fringilla tortor elit, quis lacinia est molestie id. Nullam consectetur elit at porta faucibus. In feugiat, mauris ut dignissim ultrices, odio ex viverra ante, ac pulvinar libero mauris facilisis diam. Fusce at efficitur urna, in varius nisl. Pellentesque lectus odio, pellentesque sed lacinia eu, placerat et mauris. Sed id mi at dui aliquet dapibus non nec justo.
+This isn't Lending Club data, just an example of 
+[Plotly partial dependence plots in Plotly](https://plot.ly/scikit-learn/plot-partial-dependence/)"""), 
 
-Phasellus sodales vehicula nisi sit amet tincidunt. Integer mattis, lacus vitae tempor congue, tellus nulla congue lectus, id facilisis enim nunc eu arcu. Suspendisse vehicula metus non urna congue, ac fringilla dolor venenatis. Nulla facilisi. Etiam ornare ipsum id massa dictum, sit amet porttitor neque vehicula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam a purus vulputate leo egestas consectetur in nec augue.
-""")
+dcc.Graph(id='explain-graph', figure=fig)
+]
