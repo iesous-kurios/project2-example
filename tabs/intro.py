91c7a19110ec2a4d8bc5f60c2887d7475b184863
iesous-kurios <j.wylie.81@gmail.com>
2019-12-19 19:16:36 -0800

    first small edits

--- tabs/intro.py
+++ tabs/intro.py
@@ -7,16 +7,7 @@ from app import app
 layout = [dcc.Markdown("""
 ### Intro
 
-This web app enables you to get a quick, anonymous estimate of what interest rate you could get on
-a Lending Club peer-to-peer loan.
-
-Lending Club will give you an estimate directly, but you have to provide your name, and they pull your
-credit score. (They say that this does not impact your credit score.)
-
-For an anonymous convenient estimate, I reverse-engineered the Lending Club formula using their public
-data and an xgboost model.
-
-This also enables you to do interactive "what-if" analysis!
+This webapp will let you predict exit destinations of guest based off of several aspects that typically present when new guests arrive.  
 """), 
 
-html.Img(src='/assets/fabian-blank-pElSkGRA2NU-unsplash.jpg', style={'width':'100%'})]
+html.Img(src='/assets/fpspokane.png', style={'width':'100%'})]
