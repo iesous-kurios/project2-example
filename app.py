21b354038f7b2bae4bb502b2789bf95a3553bfef
iesous-kurios <j.wylie.81@gmail.com>
2019-12-19 20:53:12 -0800

    changed tab name of app

--- app.py
+++ app.py
@@ -4,4 +4,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.config.suppress_callback_exceptions = True
+
+app.title = 'FPSpokane App' # appears in browser title bar
+
 server = app.server
