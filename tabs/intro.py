25af0064802123124f16d8cf5e9c8018e836bf45
iesous-kurios <j.wylie.81@gmail.com>
2019-12-24 15:41:08 -0800

    made small change

--- tabs/intro.py
+++ tabs/intro.py
@@ -7,7 +7,7 @@ from app import app
 layout = [dcc.Markdown("""
 ### Intro
 
-This webapp will let you predict exit destinations of guest based off of several aspects that typically present when new guests arrive.  
-"""), 
+This webapp will let you predict exit destinations of guest based off of several aspects that typically present when new guests arrive at the shelter.  Please use the sliders to match the current condition  
+""), 
 
 html.Img(src='/assets/fpspokane.png', style={'width':'100%'})]
