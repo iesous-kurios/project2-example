45e892df0de32b2aacf8954306161bbd5994c02d
iesous-kurios <j.wylie.81@gmail.com>
2019-12-25 21:22:09 -0800

    fixed small error

--- tabs/intro.py
+++ tabs/intro.py
@@ -8,6 +8,6 @@ layout = [dcc.Markdown("""
 ### Intro
 
 This webapp will let you predict exit destinations of guest based off of several aspects that typically present when new guests arrive at the shelter.  Please use the sliders to match the current condition  
-""), 
+"""), 
 
 html.Img(src='/assets/fpspokane.png', style={'width':'100%'})]
