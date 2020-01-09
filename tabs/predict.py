a04ca3454003b146c6ab980f05f862269d6c2218
iesous-kurios <j.wylie.81@gmail.com>
2020-01-08 21:19:16 -0800

    changes to model file

--- tabs/predict.py
+++ tabs/predict.py
@@ -35,59 +35,51 @@ layout = html.Div([
     html.Div(id='prediction-content', style={'fontWeight':'bold'}), 
 
     html.Div([
-        dcc.Markdown('###### Annual Income'), 
+        dcc.Markdown('###### Length of Stay'), 
         dcc.Slider(
-            id='annual-income', 
-            min=20000,
-            max=200000,
-            step=5000,
-            value=65000, 
-            marks={n: f'{n/1000:.0f}k' for n in range(20000,220000,20000)} 
+            id='bednights', 
+            min=0,
+            max=50,
+            step=5,
+            value=30, 
+            marks={n: str(n) for n in range(0,50,5)}
         ), 
     ], style=style), 
 
     html.Div([
-        dcc.Markdown('###### Credit Score'), 
+        dcc.Markdown('###### Income at Entry'), 
         dcc.Slider(
-            id='credit-score', 
-            min=650,
-            max=850, 
-            step=10, 
-            value=700, 
-            marks={n: str(n) for n in range(650,900,50)}
+            id='income', 
+            min=0,
+            max=2000, 
+            step=200, 
+            value=800, 
+            marks={n: str(n) for n in range(0,2000,200)}
         ),
     ], style=style), 
 
     html.Div([
-        dcc.Markdown('###### Loan Amount'), 
+        dcc.Markdown('###### Total Household Size'), 
         dcc.Slider(
-            id='loan-amount', 
-            min=1000, 
-            max=40000, 
-            step=1000, 
-            value=10000, 
-            marks={n: f'{n/1000:.0f}k' for n in range(5000,45000,5000)}
+            id='casemembers', 
+            min=1, 
+            max=10, 
+            step=1, 
+            value=3, 
+            marks={n: str(n) for n in range(0,10,1)}
         ),  
     ], style=style),
 
-    html.Div([
-        dcc.Markdown('###### Loan Purpose'), 
-        dcc.Dropdown(
-            id='loan-purpose', 
-            options=[{'label': purpose, 'value': purpose} for purpose in loan_purposes], 
-            value=loan_purposes[0]
-        ), 
-    ], style=style),
 
     html.Div([
-        dcc.Markdown('###### Monthly Debts'), 
+        dcc.Markdown('###### Contact Services'), 
         dcc.Slider(
-            id='monthly-debts', 
+            id='services', 
             min=0, 
-            max=5000, 
-            step=100, 
-            value=1000, 
-            marks={n: str(n) for n in range(500,5500,500)}
+            max=500, 
+            step=20, 
+            value=200, 
+            marks={n: str(n) for n in range(0,500,20)}
         )
     ], style=style),
 
@@ -95,20 +87,19 @@ layout = html.Div([
 
 @app.callback(
     Output('prediction-content', 'children'),
-    [Input('annual-income', 'value'),
-     Input('credit-score', 'value'),
-     Input('loan-amount', 'value'),
-     Input('loan-purpose', 'value'),
-     Input('monthly-debts', 'value')])
-def predict(annual_income, credit_score, loan_amount, loan_purpose, monthly_debts):
+    [Input('bednights', 'value'),
+     Input('income', 'value'),
+     Input('casemembers', 'value'),
+     Input('services', 'value')])
+def predict(bednights, income, casemembers, services):
 
     df = pd.DataFrame(
-        columns=['Annual Income', 'Credit Score', 'Loan Amount', 'Loan Purpose', 'Monthly Debts'], 
-        data=[[annual_income, credit_score, loan_amount, loan_purpose, monthly_debts]]
+        columns=['Total Bednights', 'Income at Entry', 'Total Household Size', 'Total Contact Services'], 
+        data=[[bednights, income, casemembers, services]]
     )
 
     pipeline = load('model/pipeline.joblib')
     y_pred_log = pipeline.predict(df)
     y_pred = np.expm1(y_pred_log)[0]
 
-    return f'{y_pred:.2f}% interest rate predicted for 36 month Lending Club loan'
+    return f'Client had permanent exit:{y_pred:.2f}'
