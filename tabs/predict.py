4afdddcb149d7920908f77ce99f5355d0d729109
iesous-kurios <j.wylie.81@gmail.com>
2020-01-09 20:53:43 -0800

    trying to get model to predict

--- tabs/predict.py
+++ tabs/predict.py
@@ -9,7 +9,7 @@ import pandas as pd
 
 from app import app
 
-layout = [
+layout = 
 column1 = dbc.Col(
     [
         dcc.Markdown('## Predictions', className='mb-5'), 
@@ -68,4 +68,3 @@ def predict(entry_income, length_homeless, CaseMembers):
     )
     y_pred = pipeline.predict(df)[0]
     return f'{y_pred:.0f} years'
-]
