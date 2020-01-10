5b9c0bad7c2ca578c5e64538c268dc620484e69c
iesous-kurios <j.wylie.81@gmail.com>
2020-01-09 20:36:11 -0800

    trying to get model to predict

--- tabs/predict.py
+++ tabs/predict.py
@@ -1,87 +1,58 @@
-from dash.dependencies import Input, Output
-import dash_core_components as dcc
-import dash_html_components as html
-
-from joblib import load
-import numpy as np
-import pandas as pd
-
-from app import app
-
-
-
-style = {'padding': '1.5em'}
-
-layout = html.Div([
-    dcc.Markdown("""
-        ### Predict
-
-        Use the controls below to update your predicted exit destination, based on length of stay, 
-        income at entry, total household members, and total contact services.
-    
-    """), 
-    
-    
-    
-    
-
-    html.Div(id='prediction-content', style={'fontWeight':'bold'}), 
-
-    html.Div([
-        dcc.Markdown('###### Length of time homeless'), 
-        dcc.Slider(
-            id='length_homeless', 
-            min=0,
-            max=200,
-            step=5,
-            value=30, 
-            marks={n: str(n) for n in range(0,200,5)}
-        ), 
-    ], style=style), 
-
-    html.Div([
-        dcc.Markdown('###### Income at Entry'), 
+column1 = dbc.Col(
+    [
+        dcc.Markdown('## Predictions', className='mb-5'), 
+        dcc.Markdown('#### Income at Entry'), 
         dcc.Slider(
             id='entry_income', 
-            min=0,
-            max=2000, 
-            step=200, 
-            value=800, 
-            marks={n: str(n) for n in range(0,2000,200)}
-        ),
-    ], style=style), 
-
-    html.Div([
-        dcc.Markdown('###### Total Household Size'), 
+            min=0, 
+            max=3000, 
+            step=100, 
+            value=2000, 
+            marks={n: str(n) for n in range(0,3000,100)}, 
+            className='mb-5', 
+        ), 
+        dcc.Markdown('#### Length of Time Homeless in days'), 
+        dcc.Slider(
+            id='length_homeless', 
+            min=0, 
+            max=400, 
+            step=10, 
+            value=40, 
+            marks={n: str(n) for n in range(0,400,10)}, 
+            className='mb-5', 
+        ), 
+        
+        dcc.Markdown('#### Total HouseHold Size'), 
         dcc.Slider(
             id='CaseMembers', 
             min=1, 
             max=10, 
             step=1, 
             value=3, 
-            marks={n: str(n) for n in range(0,10,1)}
-        ),  
-    ], style=style),
+            marks={n: str(n) for n in range(1,10,1)}, 
+            className='mb-5', 
+        ), 
+    ],
+    md=4,
+)
 
+column2 = dbc.Col(
+    [
+        html.H2('Exit To Permanent Housing', className='mb-5'), 
+        html.Div(id='prediction-content', className='lead')
+    ]
+)
 
-])
+import pandas as pd
 
 @app.callback(
     Output('prediction-content', 'children'),
-    [Input('length_homeless)', 'value'),
-     Input('entry_income', 'value'),
-     Input('CaseMembers', 'value')])
-def predict(length_homeless, entry_income, CaseMembers):
-
+    [Input('entry_income', 'value'), Input('length_homeless', 'value'), Input('CaseMembers', 'value'],
+)
+def predict(entry_income, length_homeless, CaseMembers):
     df = pd.DataFrame(
-        columns=['length_homeless', 'entry_income', 'CaseMembers'], 
-        data=[[length_homeless, entry_income, CaseMembers]]
+        columns=['entry_income', 'length_homeless', 'CaseMembers'], 
+        data=[[entry_income, length_homeless, CaseMembers]]
     )
-
-    pipeline = load('model/pipeline.joblib')
     y_pred = pipeline.predict(df)[0]
-    
-
-    return f'{y_pred:.0f} interest rate predicted for 36 month Lending Club loan'
- 
- 
+    return f'{y_pred:.0f} years'
